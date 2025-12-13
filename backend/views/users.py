8e59144ed834553c952a51fbcacc4f8f73c9b132
Grace Nduta <ndutak5674@gmail.com>
2025-12-13 23:41:16 +0500

    User getting all shoes

--- backend/views/users.py
+++ backend/views/users.py
@@ -1,7 +1,21 @@
 from flask import Blueprint, request, jsonify
-from models import db, User
+from models import db, User, Shoe
 
-users_bp = Blueprint('users', __name__, url_prefix='/users')
+users_bp = Blueprint('users', __name__, url_prefix='/api')
+
+@users_bp.route('/shoes', methods=['GET'])
+def get_shoes():
+    shoes =Shoe.query.all()
+    return jsonify([{
+        'id': shoe.id,
+        'name': shoe.name,
+        'size': shoe.size,
+        'color': shoe.color,
+        'price': shoe.price
+    } for shoe in shoes]), 200
+
+
+
 
 
     
